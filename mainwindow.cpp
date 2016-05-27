959509e269f1d8b41543bf1f83e7bbab89241c8e
Domingos <domingosliraneto@hotmail.com>
2016-05-26 22:33:27 -0300

    Atualizacaocliente
    Foi adicionado o sistema de comunicação TCP/IP. cliente.cpp e cliente.h

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,4 +11,34 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     delete ui;
+    delete this-> wifi;
+}
+
+void MainWindow::on_botaoConectar_clicked()
+{
+    this->wifi = new Client(ui->lineIP->text(), ui->linePorta->text().toInt());
+    connect(wifi, SIGNAL(connectionSuccessful()), this, SLOT(SucessoNaConexao()));
+    connect(wifi, SIGNAL(hasReadData()), this, SLOT(RecebimentoDosDados()));
+}
+
+void MainWindow::on_enviarDados_clicked()
+{
+    this->wifi->writeData(ui->lineEnviarDados->text());
+}
+
+void MainWindow::on_botaoDesconectar_clicked()
+{
+
+}
+
+void MainWindow::SucessoNaConexao()
+{
+    QMessageBox msgBox;
+    msgBox.setText("Conexão Realizada com sucesso");
+    msgBox.exec();
+}
+
+void MainWindow::RecebimentoDosDados()
+{
+   ui->lineDadosRecebidos->setText(wifi->dataReceived());
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QmessageBox>
+#include "client.h"
 
 namespace Ui {
 class MainWindow;
@@ -15,8 +17,21 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
 
+private slots:
+
+    void on_botaoConectar_clicked();
+
+    void on_enviarDados_clicked();
+
+    void on_botaoDesconectar_clicked();
+
+    void RecebimentoDosDados();
+
+    void SucessoNaConexao();
+
 private:
     Ui::MainWindow *ui;
+    Client *wifi;
 };
 
 #endif // MAINWINDOW_H
