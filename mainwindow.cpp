56982a00fbb523b80929f6ef67df48c4c556c7b7
Domingos <domingosliraneto@hotmail.com>
2016-06-08 22:15:40 -0300

    Atualization

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    PlotHandler::plot<double> T("1,2,3,4,5","1,2,3,4,5",ui-> grafico2);
+    //PlotHandler::plot<double> T("1,2,3,4,5","1,2,3,4,5",ui-> grafico2);
     //P[0] = new PlotHandler::plot<double>(ui-> grafico2);
     P = new PlotHandler::plot<double>(ui->grafico1);
     //Time = 0;
@@ -64,7 +64,7 @@ MainWindow::~MainWindow()
 
 void MainWindow::on_botaoConectar_clicked()
 {
-    this->wifi = new Client(ui->lineIP->text(), ui->linePorta->text().toInt());
+    this->wifi = new Client(ui->IP->text(), ui->Porta->text().toInt());
     connect(wifi, SIGNAL(connectionSuccessful()), this, SLOT(SucessoNaConexao()));
     connect(wifi, SIGNAL(hasReadData()), this, SLOT(RecebimentoDosDados()));
 
